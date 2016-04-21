2fb95014bb43fdb18d609218015756649b22d692
Aleksander Bai <ab@prognett.no>
2016-04-21 14:37:05 +0200

    Added code for extracting entities

--- 02_prepare_data.sh
+++ 02_prepare_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Preparing singal media corpus"
-python scripts/extract_json.py data/signalmedia-1m.jsonl data/signalmedia.data
+echo "Splitting signal media corpus into months"
+#mkdir data/signal_split/
+#python scripts/split_data.py data/signalmedia-1m.jsonl data/signal_split/
+
+echo "Preparing data"
+#python scripts/prepare_data.py data/signal_split/signalmedia-1m.split.y2015.m7 data/signal_split/signalmedia-1m.split.y2015.m7.prepared
+
+echo "Extracting entities"
+python scripts/extract_entities.py data/signal_split/signalmedia-1m.split.y2015.m7 data/signal_split/signalmedia-1m.split.y2015.m7.entities
--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -10,9 +10,18 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
+from nltk.tag.perceptron import PerceptronTagger
 
+tagger = PerceptronTagger()
 cachedStopWords = stopwords.words("english")
 lemmatizer = WordNetLemmatizer()
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
@@ -47,7 +56,7 @@ def lemmatize(input):
     for sentence in sentences:
         tokenized = nltk.word_tokenize(sentence)
         tokenized = removeStopWords(tokenized)
-        tagged = nltk.pos_tag(tokenized)
+        tagged = nltk.tag._pos_tag(tokenized, None, tagger)
 
         for word in tagged:
             words.append(lemmatizer.lemmatize(word[0], get_wordnet_pos(word[1])))
@@ -70,14 +79,23 @@ if __name__ == '__main__':
         sys.exit(1)
     inp, outp = sys.argv[1:3]
 
-    output = open(outp, 'w');
+    output = open(outp, 'w')
+    length = num_lines = sum(1 for line in open(inp))
+    logger.info("Processing %s that has %i lines", inp, length)
     with open(inp) as f:
+        counter = 0.0
         for line in f:
             obj = json.loads(line)
             obj["content"] = lemmatize(obj["content"])
             obj["title"] = lemmatize(obj["title"])
             output.write(json.dumps(obj, ensure_ascii=False).encode("utf8"))
+            output.write('\n')
+            counter += 1.0
+
+            if ((counter % 10) == 0):
+                sys.stdout.write("Processed : %.2f   \r" % (100 * (counter / length)))
+                sys.stdout.flush()
 
     output.close()
 
-    print "Done"
+    logger.info("Done")
